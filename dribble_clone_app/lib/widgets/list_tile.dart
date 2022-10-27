da724ff7a65e1d863f09a66c67cc999c8eed9d17
masupa <dmasupa4@gmail.com>
2022-10-27 23:00:47 +0200

    improved list tile

--- dribble_clone_app/lib/widgets/list_tile.dart
+++ dribble_clone_app/lib/widgets/list_tile.dart
@@ -1,29 +1,53 @@
 import 'package:flutter/material.dart';
 
+import '../misc/colors.dart';
 import 'icon_container.dart';
 import 'large_text.dart';
 import 'small_text.dart';
 
 class Tile extends StatelessWidget {
-  const Tile({Key? key}) : super(key: key);
+  // Attributes
+  final IconData icon;
+  final Color containerColor;
+  final String title;
+  final String subtitle;
+
+  // Constructor
+  const Tile({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    this.containerColor = AppColors.containerColor,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10),
+      // Attributes
+      padding: const EdgeInsets.symmetric(vertical: 15),
       margin: const EdgeInsets.only(bottom: 20),
+
+      // Container Child
       child: ListTile(
-        leading: IconContainer(icon: Icons.emoji_emotions),
+        leading: IconContainer(
+          width: 60,
+          height: 60,
+          icon: icon,
+          containerColor: containerColor,
+        ),
+        // Title & Subtitle
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            LargeText(text: 'Speaking Skills', textSize: 17.5),
-            const SizedBox(height: 5.0),
-            SmallText(text: '16 Exercises'),
+            LargeText(text: title, textSize: 17.5),
+            const SizedBox(height: 10),
+            SmallText(text: subtitle),
           ],
         ),
         trailing: const Icon(Icons.more_horiz),
       ),
+      // Container decoration
       decoration: BoxDecoration(
         color: const Color(0xFFf2f8ff),
         borderRadius: BorderRadius.circular(25.0),
