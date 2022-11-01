3576fba61da1110f8e500bb4acd3146494817201
masupa <dmasupa4@gmail.com>
2022-11-01 22:41:10 +0200

    Added an ImageTile widget

--- dribble_clone_app/lib/widgets/list_tile.dart
+++ dribble_clone_app/lib/widgets/list_tile.dart
@@ -5,7 +5,7 @@ import 'icon_container.dart';
 import 'large_text.dart';
 import 'small_text.dart';
 
-class Tile extends StatelessWidget {
+class IconTile extends StatelessWidget {
   // Attributes
   final IconData icon;
   final Color containerColor;
@@ -13,7 +13,7 @@ class Tile extends StatelessWidget {
   final String subtitle;
 
   // Constructor
-  const Tile({
+  const IconTile({
     Key? key,
     required this.icon,
     required this.title,
@@ -40,7 +40,62 @@ class Tile extends StatelessWidget {
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            LargeText(text: title, textSize: 17.5),
+            LargeText(text: title, textSize: 15.0),
+            const SizedBox(height: 10),
+            SmallText(text: subtitle),
+          ],
+        ),
+        trailing: const Icon(Icons.more_horiz),
+      ),
+      // Container decoration
+      decoration: BoxDecoration(
+        color: const Color(0xFFf2f8ff),
+        borderRadius: BorderRadius.circular(25.0),
+      ),
+    );
+  }
+}
+
+class ImageTile extends StatelessWidget {
+  // Attributes
+  final String imageAssetName;
+  final Color containerColor;
+  final String title;
+  final String subtitle;
+
+  // Constructor
+  const ImageTile({
+    Key? key,
+    required this.imageAssetName,
+    required this.title,
+    required this.subtitle,
+    this.containerColor = AppColors.containerColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Attributes
+      padding: const EdgeInsets.symmetric(vertical: 15),
+      margin: const EdgeInsets.only(bottom: 20),
+
+      // Container Child
+      child: ListTile(
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: Image.asset(
+            'assets/images/$imageAssetName.jpg',
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        // Title & Subtitle
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            LargeText(text: title, textSize: 15.0),
             const SizedBox(height: 10),
             SmallText(text: subtitle),
           ],
