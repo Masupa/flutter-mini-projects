501842c582ced6da9225bd1b5586806d70bc55ae
masupa <dmasupa4@gmail.com>
2022-11-08 01:17:54 +0200

    Modified small text widget to have color property

--- cashback_app/lib/widgets/small_text.dart
+++ cashback_app/lib/widgets/small_text.dart
@@ -3,17 +3,22 @@ import 'package:flutter/material.dart';
 class SmallText extends StatelessWidget {
   final String text;
   final double textSize;
+  final Color textColor;
 
   // Constructor
-  const SmallText({Key? key, required this.text, this.textSize = 22.5})
-      : super(key: key);
+  const SmallText({
+    Key? key,
+    required this.text,
+    this.textSize = 22.5,
+    this.textColor = const Color.fromARGB(126, 253, 255, 255),
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       style: TextStyle(
-        color: const Color.fromARGB(126, 253, 255, 255),
+        color: textColor,
         fontSize: textSize,
       ),
     );
