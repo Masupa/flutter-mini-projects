bb4add6c4977d32ff9c315dd7a74f19f52787843
masupa <dmasupa4@gmail.com>
2022-11-08 00:18:52 +0200

    Added colors class

---
+++ cashback_app/lib/misc/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color mainColor = Color(0xFF121418);
+  static const Color selectColorLight = Color(0xFFd8ff64);
+  static const Color primaryTextColor = Color(0xFFfdffff);
+  static const Color secondaryTextColor = Color(0xFF8e8f93);
+  static const Color containerColor = Color(0xFF1c1d21);
+  static const Color thirdTextColor = Color(0xFF1c1d21);
+}
