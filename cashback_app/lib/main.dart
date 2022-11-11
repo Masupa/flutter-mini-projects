89bb2600bfbf6200bf5bebd237f946dc3f40e735
masupa <dmasupa4@gmail.com>
2022-11-11 13:31:17 +0200

    Updated routes: added cart page route

--- cashback_app/lib/main.dart
+++ cashback_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cashback_app/pages/item.dart';
 import 'package:cashback_app/pages/stores.dart';
 import 'package:cashback_app/pages/home.dart';
+import 'package:cashback_app/pages/cart.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -19,7 +20,8 @@ class CashbackApp extends StatelessWidget {
       routes: {
         "/": (context) => const HomePage(),
         "/store_page": (context) => const StoresPage(),
-        "/item_page": (context) => const ItemPage()
+        "/item_page": (context) => const ItemPage(),
+        "/cart_page": (context) => const CartPage(),
       },
       theme: ThemeData(
         textTheme: GoogleFonts.openSansTextTheme(
