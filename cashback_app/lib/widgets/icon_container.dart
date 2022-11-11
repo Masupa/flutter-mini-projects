097129befeb95c20e60fa291d59fb24ec99ad462
masupa <dmasupa4@gmail.com>
2022-11-11 13:17:54 +0200

    Added named route and elevation to icon container

--- cashback_app/lib/widgets/icon_container.dart
+++ cashback_app/lib/widgets/icon_container.dart
@@ -7,21 +7,36 @@ class IconContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // properties
-      width: 75,
-      height: 75,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
+    return InkWell(
+      borderRadius: BorderRadius.circular(30),
+      onTap: () {
+        Navigator.pushNamed(context, '/');
+      },
+      child: Material(
+        elevation: 50.0,
         borderRadius: BorderRadius.circular(30),
         color: AppColors.containerColor,
-      ),
+        shadowColor: Colors.black,
+        child: Container(
+          // properties
+          width: 75,
+          height: 75,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: AppColors.containerColor,
+          ),
 
-      // Icon child
-      child: Icon(
-        icon,
-        color: AppColors.primaryTextColor,
+          // Icon child
+          child: Icon(
+            icon,
+            color: AppColors.primaryTextColor,
+          ),
+        ),
       ),
     );
   }
 }
+
+// Navigate back to home page
+        // Navigator.pushNamed(context, '/');
