c6e2caf10a985588384a42c8d6a66a2bd02960eb
masupa <dmasupa4@gmail.com>
2022-11-15 21:06:26 +0200

    Updated to take `currentPage` and route to prev page

--- cashback_app/lib/widgets/dynamic_app_bar.dart
+++ cashback_app/lib/widgets/dynamic_app_bar.dart
@@ -3,38 +3,59 @@ import 'package:flutter/material.dart';
 import 'icon_container.dart';
 
 class CustomAppBar extends StatelessWidget {
-  final bool isHome;
+  final String currentPage;
   final IconData icon;
+
   const CustomAppBar({
     Key? key,
-    this.isHome = true,
+    this.currentPage = "home",
     required this.icon,
   }) : super(key: key);
 
+  Widget getAppBar() {
+    if (currentPage == "store") {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const IconContainer(
+            icon: Icons.arrow_back,
+            navRoute: "/",
+          ),
+          IconContainer(
+            icon: icon,
+            navRoute: "/cart_page",
+          ),
+        ],
+      );
+    } else if (currentPage == "item") {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const IconContainer(
+            icon: Icons.arrow_back,
+            navRoute: "/store_page",
+          ),
+          IconContainer(
+            icon: icon,
+            navRoute: "/cart_page",
+          ),
+        ],
+      );
+    } else {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          IconContainer(
+            icon: icon,
+            navRoute: "/",
+          )
+        ],
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return isHome
-        ? Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconContainer(
-                icon: icon,
-                navRoute: "/",
-              )
-            ],
-          )
-        : Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const IconContainer(
-                icon: Icons.arrow_back,
-                navRoute: "/",
-              ),
-              IconContainer(
-                icon: icon,
-                navRoute: "/cart_page",
-              ),
-            ],
-          );
+    return getAppBar();
   }
 }
